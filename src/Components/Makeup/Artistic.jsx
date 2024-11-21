f3aa3f80ebd8f7520f5651c273c2f103b9160829
Carlos <carlosscristancho69@gmail.com>
2024-11-21 18:50:26 -0500

    primer boceto de la pagina artistic

--- src/Components/Makeup/Artistic.jsx
+++ src/Components/Makeup/Artistic.jsx
@@ -4,6 +4,10 @@ import Footer from "../../Common/Footer/Footer";
 
 import mainImg from "../../Common/Images/carousel1.jpg"
 import quinciañera from "../../Common/Images/mainMakeup.jpg"
+import artistic1 from "../../Common/Images/artistic1.jpg"
+import artistic2 from "../../Common/Images/artistic2.jpg"
+import artistic3 from "../../Common/Images/artistic3.jpg"
+import artistic4 from "../../Common/Images/artistic4.jpg"
 
 const Makeup = () => {
 
@@ -30,8 +34,8 @@ const Makeup = () => {
     return (
         <>
             <Header />
-            <div className="row justify-content-center">
-                <div className="Artistic col-11">
+            <div className="artistic row">
+                {/* <div className="Artistic col-11">
                     <div className="artisticContainer row justify-content-center">
                         <div className="imgContiainer col-12 col-sm-5">
                             <img className="img" src={mainImg} alt="mainIMage" />
@@ -60,7 +64,14 @@ const Makeup = () => {
                             )
                         })}
                     </div>
-                </div>
+                </div> */}
+
+                <h2 className="col-12">Maquillaje artistico</h2>
+                <img className="col-11 col-sm-8 col-md-5 col-lg-4" src={artistic1} alt="artistic"/>
+                <img className="col-11 col-sm-8 col-md-5 col-lg-4" src={artistic2} alt="artistic"/>
+                <img className="col-11 col-sm-8 col-md-5 col-lg-4" src={artistic3} alt="artistic"/>
+                <img className="col-11 col-sm-8 col-md-5 col-lg-4" src={artistic4} alt="artistic"/>
+
             </div>
             <Footer/>
         </>
